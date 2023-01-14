5498235a94d9d9ea1f0580b3f8b3f91ef027540d
Alexandre-200 <alexandre_200@outlook.com.br>
2023-01-13 23:16:31 -0300

    update

--- src/core/components/Sidebar.js
+++ src/core/components/Sidebar.js
@@ -1,7 +1,7 @@
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <h1>Sidebar</h1>
+      <div className="sidebar__logo"></div>
     </div>
   );
 };
--- src/core/pages/Home.js
+++ src/core/pages/Home.js
@@ -6,6 +6,7 @@ const Home = () => {
   return (
     <div className="home">
       <Sidebar />
+    
       <Content>
         <Topbar />
       </Content>
