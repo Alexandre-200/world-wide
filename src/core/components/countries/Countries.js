5c8c5b275af5cc3bab1376570a65ad3d27973d43
Alexandre-200 <alexandre_200@outlook.com.br>
2023-01-15 09:24:49 -0300

    update country

--- src/core/components/countries/Countries.js
+++ src/core/components/countries/Countries.js
@@ -1,11 +1,14 @@
 import Country from "./Country";
 
 const Countries = () => {
+  const countries = Array.from(Array(6).keys());
   return (
     <div className="countries">
       <div className="countries__search">Barra de pesquisa</div>
       <div className="countries__list">
-        <Country />
+        {countries.map((country, i) => {
+          return <Country key={i} />;
+        })}
       </div>
       <div className="countries__pagination">lista de paises</div>
     </div>
