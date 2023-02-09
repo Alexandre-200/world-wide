3fdbce2f36c81ea839df9e4e49786265907de123
Alexandre-200 <alexandre_200@outlook.com.br>
2023-02-09 10:31:16 -0300

    update rotes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import State from "./core/pages/State";
 const App = () => {
   return (
     <Routes>
-      <Route path="/world-wide/" element={<Home />} />
+      <Route path="/world-wide" element={<Home />} />
       <Route path="/states" element={<State/>} />
       <Route path="/country/:isoCode" element={<IsCountry />} />
       <Route path="/state/:isoCode" element={<IsState />} />
