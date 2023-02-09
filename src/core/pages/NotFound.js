169695d9aae40f0957d150bd358d41350fe888ee
Alexandre-200 <alexandre_200@outlook.com.br>
2023-02-09 14:03:36 -0300

    update to

--- src/core/pages/NotFound.js
+++ src/core/pages/NotFound.js
@@ -4,7 +4,7 @@ const NotFound = () => {
   return (
     <div className="not-found">
       <h1 className="not-found__header">Pagina nao encontrada!</h1>
-      <Link className="not-found__link" to="/">Voltar para pagina incial </Link>
+      <Link className="not-found__link" to="/world-wide/">Voltar para pagina incial </Link>
     </div>
   );
 };
